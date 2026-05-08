d620bdb955f5b7e7b99b343c47fe69cda5334bc9
aravindsuluguri <121874459+aravindsuluguri@users.noreply.github.com>
2026-05-08 18:13:23 +0530

    fix

--- backend/app.py
+++ backend/app.py
@@ -269,5 +269,10 @@ def serve_results_image(filename):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "5001"))
-    app.run(debug=True, port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False,
+        use_reloader=False
+    )
